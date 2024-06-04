cc8784a78e9d0b1cbc36e3a07f31c6d1f85c2352
Lokrag <39856609+Lokrag@users.noreply.github.com>
2024-06-04 21:50:41 +0200

    Add files via upload

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -3,65 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oalsadi- <oalsadi-@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: oalsadi- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/18 18:11:10 by oalsadi-          #+#    #+#             */
-/*   Updated: 2024/05/18 18:11:10 by oalsadi-         ###   ########.fr       */
+/*   Created: 2024/05/31 20:29:27 by oalsadi-          #+#    #+#             */
+/*   Updated: 2024/05/31 20:29:30 by oalsadi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)//encabezado
-//{	//inicializamos variables
-	//si el file descriptor es menor a 0 o el buffer es menor a 1
-		//retornamos NULL
-	//contador de caracteres leidos
-	//inicializamos un buffer
-	//inicializamos una cadena
-	//mientras el buffer sea mayor a 0
-		//leemos el file descriptor
-		//si no se leyo nada
-			//liberamos memoria
-			//retornamos NULL
-		//si se leyo algo
-			//guardamos en una cadena la linea leida
-			//si se encontro un salto de linea
-				//guardamos en una cadena la linea leida
-				//liberamos memoria
-				//retornamos la linea leida
-		//si no se encontro un salto de linea
-			//guardamos en una cadena la linea leida
-		//leer asta encontrar un salto de linea o final del archivo
-	//guardamos en una cadena la linea leida strncpy
-	//si llegas al final del archivo o no se leyo nada
-		//liberamos memoria
-		//retornamos NULL
-	//si se leyo algo
-		//guardamos en una cadena la linea leida
-		//liberamos memoria
-		//retornamos la linea leida
-//}
-
-//encabezado de la funcion que encuentra el caracter de salto de linea
-
-size_t	strlen(const *str)// encabezado buscamos la longitud de una cadena
-//inicializamos un contador 
+char	*read_from_file(int fd, char *buffer)
 {
-	size_t	len;
+	int	ret;
 
-	len = 0;
-	if(str[len] == NULL)
+	ret = read(fd, buffer, BUFFER_SIZE);
+	if (ret == -1)
 	{
+		free(buffer);
 		return (NULL);
 	}
-	else
-	{
-		len++;
-	}
-	return (len);
+	buffer[ret] = '\0';
+	return (buffer);
 }
-//si la cadena es diferente de NULL
-	//Mientras sea asi
-		// incrementamos el contador
-	//retornamos el contador
+
+char	*check_newline(char *buffer, char *line)
+{
+	char	*temp;
+
+	while (strchr (buffer, '\n' == -1))
+	{
+		temp = strdup(line);
+		free(line);
+		line = strjoin(temp, buffer);
+		free(temp);
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	char	*line;
+	char	*buffer;
+
+	buffer = (char *)malloc(BUFFER_SIZE +1);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	buffer = read_from_file(fd, buffer);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	line = strdup("");
+	line = check_newline(buffer, line);
+	free(buffer);
+	return (line);
+}
+
--- gnl/get_next_line_utils.c
+++ gnl/get_next_line_utils.c
@@ -12,12 +12,84 @@
 
 #include "get_next_line.h"
 
-char    *strchr(const char *str, int c)
+size_t	strlen(const char *str)
 {
-    unsigned char   a;
-    int				b;
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
+char	*strchr(const char *str, int c)
+{
+	int	i;
 
-strchr;
-strcpy;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+		{
+			return ((char *)str + i);
+		}
+		i++;
+	}
+	if (c == '\0')
+	{
+		return ((char *)str + i);
+	}
+	return (NULL);
+}
+
+char	*strjoin(const char *s1, const char *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc(strlen(s1) + strlen(s2) + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+		free(str);
+	}
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (*s2 != '\0')
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*strdup(const char *s1)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = (char *)malloc(strlen(s1) + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+		free(str);
+	}
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
